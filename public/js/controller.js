0e9a683672f6f6607064d2321f14329ac73fa991
John Gibbs <jgibbs@enphaseenergy.com>
2013-12-10 10:19:10 -0800

    clean up

--- public/js/controller.js
+++ public/js/controller.js
@@ -74,7 +74,6 @@ angular.module('i-like-solar').controller('fbCtrl',
 		 picture: 'http://www.seia.org:3000/img/sun.png',
 		 caption: 'Share your story',
 		 description: 'Solar is getting smarter! Did you know your friends could get solar 3x faster if they know you have solar? Share your solar story to help multiply solar power in your community. via I LIKE SOLAR.',
-        //picture: 'http://www.fbrell.com/public/f8.jpg'
 		};
 
 		fb.ui(publish,  function(res){
