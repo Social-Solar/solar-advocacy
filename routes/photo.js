f25d8624928419f9658e9cb932e9f2c1ff4d0e1d
Jonathan Craig <jc@insomniacdesign.com>
2014-01-31 11:23:18 -0700

    Cover banner offset change

--- routes/photo.js
+++ routes/photo.js
@@ -111,7 +111,7 @@ function _solarizeCover(file, offset, cb) {
       var logo    = new Canvas.Image();
       logo.onload = function () {
         ctx.drawImage(img, 0, 0, img.width, img.height);
-        ctx.drawImage(logo, 0, img.height-offset-50, img.width, logo.height);
+        ctx.drawImage(logo, 0, img.height-offset-100, img.width, logo.height);
         _saveCanvas(canvas, file, function (err) {
           if (err) return cb (err);
           cb();
